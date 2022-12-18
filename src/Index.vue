692534610bd298f39e84afa4aa5fb40d6748ef00
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2022-12-18 23:52:44 +0700

    Added files

---
+++ src/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <div id="main_container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Index'
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+        background: #F0F0F0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, Fira, Sans, "Droid Sans", "Helvetica Neue", sans-serif;
+    }
+</style>
